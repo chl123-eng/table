f128d6a7590f35b8851ddc299bf4360e23d0df7c
chl <123@qq.com>
2020-12-06 18:30:20 +0800

    second commit

--- src/components/box.vue
+++ src/components/box.vue
@@ -1,54 +1,186 @@
 <template>
   <div>
-    <!-- <div v-bind:style="[obj1, obj2]">11111111</div> -->
-    <!-- <div :class="[activeClass1,activeClass2]"></div>
-    <button class="btn" @click="change()">快点一下</button> -->
-    
-
-    <!-- <p @click="fullName">111</p> -->
-    <!-- <p>{{obj1.a}}</p>
-    <p>{{obj2.b}}</p>
-    <p>{{obj2.c}}</p> -->
-
-    <p v-for="(item,index) in newVal" :key='index' @click="change(index)">{{newVal[index].k}}</p>
+    <div class="header"><div class="wrap planList">任务计划列表</div></div>
+    <div class="wrap addTask">添加任务:</div>
+    <div class="wrap">
+      <input class="inputTask" type="text" placeholder="输入" v-model="msg" @keyup.enter="enter(msg, all)"/>
+    </div>
+    <div class="wrap">
+      <span class="tip">2个任务未完成</span>
+      <div class="btn" v-for="(item, index) in taskTarget" :key="index">
+        <button :key="item.k" @click="changeType(item.type)">{{ item.name }}</button>
+      </div>
+    </div>
+    <div class="taskList wrap">任务列表:</div>
+    <div class="wrap">
+      <!-- 谢树宏新增 -->
+      <div v-show="type === 'all'">
+        <div class="task" :key="index" v-for="(item, index) in all">
+          <input type="checkbox" :key="item.checked" v-model="item.checked" @click="allAdd(index)" />
+          <span :class="{ taskActive: item.checked }">{{ item.name }}</span>
+        </div>
+      </div>
+      <div v-show="type === 'finish'">
+        <div class="task" :key="index" v-for="(item, index) in finish">
+          <input type="checkbox" :key="item.checked" v-model="!item.checked" @click="fAdd(index)"/>
+<!-- 第一个问题 -->
+          <span :class="{ taskActive: !item.checked }">{{ item.name }}</span>
+        </div>
+      </div>
+      <div v-show="type === 'unfinish'">
+        <div class="task" :key="index" v-for="(item, index) in unfinish">
+          <input type="checkbox" :key="item.checked" v-model="item.checked" @click="ufAdd(index)"/>
+          <span :class="{ taskActive: item.checked }">{{ item.name }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'class_styles',
-  components: {
-    
-  },
+  name: 'todoList',
+  components: {},
   data() {
-    return{
-      newVal:[
+    return {
+      currentK: 0,
+      type: 'all',
+      taskTarget: [
         {
-          k: false
+          type: 'all',
+          k: 1,
+          name: '所有任务',
+        },
+        {
+          type: 'unfinish',
+          k: 2,
+          name: '未完成任务',
+        },
+        {
+          type: 'finish',
+          k: 3,
+          name: '已完成任务',
+        },
+      ],
+      
+      newObj: {
+        name: "",
+        checked: ""
+      },
+      
+      all: [
+        {
+          name: '学习vue',
+          checked: false,
+        },
+        {
+          name: '移动端',
+          checked: false,
+        },
+        {
+          name: '西安一日游',
+          checked: false,
         }
-      ]
-    }
+      ],
+      finish: [
+        //{
+        //   name: '移动端',
+        //   checked: true,
+        // },
+        // {
+        //   name: '西安一日游',
+        //   checked: true,
+        // }
+      ],
+      unfinish: [
+        // {
+        //   name: '学习vue',
+        //   checked: false,
+        // },
+      ],
+    };
   },
   methods: {
-    change(index) {
-     this.newVal[index].k = !this.newVal[index].k
+    changeType(type) {
+      this.type = type;
+    },
+    allAdd(index) {
+      this.all[index].k = !this.all[index].k;
+      if(this.all[index].k){
+        this.finish.push(this.all[index])
+      }
+      for(var i = 0;i < this.all.length; i++){
+        if(!this.all[i].k){
+          this.unfinish.push(this.all[i])
+        }
+      }
+    },
+    fAdd(index){
+      this.finish[index].k = !this.finish[index].k;
+      if(!this.finish[index].k){
+        this.unfinish.push(this.finish[index])
+      }
+    },
+    ufAdd(index){
+      this.unfinish[index].k = !this.unfinish[index].k;
+      if(this.unfinish[index].k){
+        this.finish.push(this.unfinish[index])
+      }
+    },
+    enter(msg,all){
+      this.newObj.checked = false;
+      this.newObj.name = msg;
+      this.all.push(this.newObj)
+      this.unfinish.push(this.newObj)
     }
   },
-  watch: {
-    newVal: {
-      handler(val, oldVal){
-        console.log(this.newVal)
-      },
-      deep: true
-    }
-  }
-}
+};
 </script>
 
 <style scoped>
-
-
-
-
+.header {
+  height: 50px;
+  background: #cc5545;
+}
+.wrap {
+  width: 1000px;
+  margin: 20px auto;
+}
+.header .planList {
+  color: #fff;
+  font-size: 20px;
+  height: 50px;
+  line-height: 50px;
+}
+.addTask {
+  font: 18px bold;
+}
+.inputTask {
+  height: 30px;
+  line-height: 30px;
+  width: 1000px;
+}
+.tip {
+  color: #cc5545;
+}
+.btn {
+  float: right;
+  height: 30px;
+  line-height: 30px;
+  border: 1px solid black;
+  margin: 0 10px;
+  border: none;
+}
+.taskList {
+  font: 18px bold;
+}
+.task {
+  border: 1px solid #ccc;
+  height: 40px;
+  line-height: 40px;
+}
+.taskActive {
+  text-decoration: line-through;
+  color: gray;
+}
 </style>
--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -2,201 +2,171 @@
   <div>
     <div class="header"><div class="wrap planList">任务计划列表</div></div>
     <div class="wrap addTask">添加任务:</div>
-    <div class="wrap"><input class="inputTask" type="text" placeholder="输入"/></div>
     <div class="wrap">
-        <span class="tip">2个任务未完成</span>
-        <div class="btn" v-for="(item,index) in taskTarget" :key="index">
-            <button :key="item.k" @click="showK(index)">{{item.name}}</button>
-        </div>
+      <input class="inputTask" type="text" placeholder="输入" v-model="msg" @keyup.enter="enter(msg, all)"/>
+    </div>
+    <div class="wrap">
+      <span class="tip">2个任务未完成</span>
+      <div class="btn" v-for="(item, index) in taskTarget" :key="index">
+        <button :key="item.k" @click="changeType(item.type)">{{ item.name }}</button>
+      </div>
     </div>
     <div class="taskList wrap">任务列表:</div>
     <div class="wrap">
-        <div class="task" :key='index' v-for="(item, index) in newTask.objs">
-            <input type="checkbox" :key='item.b' v-model="item.b" @click="add(index)"/>
-            <span :class="{taskActive: item.b}">{{ item.taskName }}</span>
+      <!-- 谢树宏新增 -->
+      <div v-show="type === 'all'">
+        <div class="task" :key="index" v-for="(item, index) in all">
+          <input type="checkbox" :key="item.checked" v-model="item.checked" @click="allAdd(index)" />
+          <span :class="{ taskActive: item.checked }">{{ item.name }}</span>
         </div>
+      </div>
+      <div v-show="type === 'finish'">
+        <div class="task" :key="index" v-for="(item, index) in finish">
+          <input type="checkbox" :key="item.checked" v-model="!item.checked" @click="fAdd(index)"/>
+<!-- 第一个问题 -->
+          <span :class="{ taskActive: !item.checked }">{{ item.name }}</span>
+        </div>
+      </div>
+      <div v-show="type === 'unfinish'">
+        <div class="task" :key="index" v-for="(item, index) in unfinish">
+          <input type="checkbox" :key="item.checked" v-model="item.checked" @click="ufAdd(index)"/>
+          <span :class="{ taskActive: item.checked }">{{ item.name }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'todoList',
-  components: {
-    
-  },
+  components: {},
   data() {
-    return{
-        currentK : 0,
-        taskTarget: [
-            {
-                k: 1,
-                name: "所有任务"
-            },
-            {
-                k: 2,
-                name: "未完成任务"
-            },
-            {
-                k: 3,
-                name: "已完成任务"
-            },
-
-        ],
-        newTask: {
-            k: "",
-            objs: [
-                // {
-                //     b: "",
-                //     taskName: ""
-                // }
-            ]
+    return {
+      currentK: 0,
+      type: 'all',
+      taskTarget: [
+        {
+          type: 'all',
+          k: 1,
+          name: '所有任务',
         },
-        tasks: [
-                {
-                    k: 1,
-                    objs: [
-                            {
-                                b: false,
-                                taskName: "学习vue"
-                            },
-                            {
-                                b: false,
-                                taskName: "移动端"
-                            },
-                            {
-                                b: false,
-                                taskName: "西安一日游"
-                            }
-                    ]
-                },
-            {
-                
-                k: 2,
-                objs: [
-                        {
-                            b: false,
-                            taskName: "学习vue"
-                        },
-                        {
-                            b: false,
-                            taskName: "移动端"
-                        }
-                ]
-        
-            },
-            {
-                k: 3,
-                objs: [
-                    {
-                        b: false,
-                        taskName: "学习vue"
-                    }
-                ]
-            }
-        ]
-    }
+        {
+          type: 'unfinish',
+          k: 2,
+          name: '未完成任务',
+        },
+        {
+          type: 'finish',
+          k: 3,
+          name: '已完成任务',
+        },
+      ],
+      
+      newObj: {
+        name: "",
+        checked: ""
+      },
+      
+      all: [
+        {
+          name: '学习vue',
+          checked: false,
+        },
+        {
+          name: '移动端',
+          checked: false,
+        },
+        {
+          name: '西安一日游',
+          checked: false,
+        }
+      ],
+      finish: [],
+      unfinish: [],
+    };
   },
   methods: {
-        add(index) {
-            this.allTasks[index].k = !this.allTasks[index].k;
-        },
-
-        // select() {
-        //      var newObj = {
-        //         k: "",
-        //         taskName: ""
-        //     };
-        //     for(var i = 0 ;i < this.allTasks.length; i++){
-        //         (function(index){
-        //             if(this.allTasks[index].k == false){
-        //                 newObj.k = false;
-        //                 newObj.taskName = this.allTasks[index].taskName
-        //                 this.noFinished.push(newObj);
-        //             }
-        //             else{
-        //                 newObj.k = true;
-        //                 newObj.taskName = this.allTasks[index].taskName
-        //                 this.finished.push(newObj);
-        //             }
-        //         })(i)
-        //     }
-        //     console.log(this.finished)
-        //     console.log(this.noFinished)
-        // }
-        showK(index){
-            this.newTask.k = this.taskTarget[index].k
-            // console.log(this.newTask.k)
-            for(var i = 0 ;i < this.tasks.length; i++){
-                if(this.newTask.k == this.tasks[i].k){
-                    this.newTask.objs = this.tasks[i].objs
-                }
-            }
-            console.log(this.newTask)
+    changeType(type) {
+      this.type = type;
+    },
+    allAdd(index) {
+      this.all[index].k = !this.all[index].k;
+      if(this.all[index].k){
+        this.finish.push(this.all[index])
+      }
+      for(var i = 0;i < this.all.length; i++){
+        if(!this.all[i].k){
+          this.unfinish.push(this.all[i])
         }
-        
-    
+      }
+    },
+    fAdd(index){
+      this.finish[index].k = !this.finish[index].k;
+      if(!this.finish[index].k){
+        this.unfinish.push(this.finish[index])
+      }
+    },
+    ufAdd(index){
+      this.unfinish[index].k = !this.unfinish[index].k;
+      if(this.unfinish[index].k){
+        this.finish.push(this.unfinish[index])
+      }
+    },
+    enter(msg,all){
+      this.newObj.checked = false;
+      this.newObj.name = msg;
+      this.all.push(this.newObj)
+      this.unfinish.push(this.newObj)
+    }
   },
-
-}
+};
 </script>
 
 <style scoped>
 .header {
-    height: 50px;
-    background: #cc5545;
+  height: 50px;
+  background: #cc5545;
 }
-
 .wrap {
-    width: 1000px;
-    margin: 20px auto
+  width: 1000px;
+  margin: 20px auto;
 }
-
 .header .planList {
-    color: #fff;
-    font-size: 20px;
-    height: 50px;
-    line-height: 50px;
+  color: #fff;
+  font-size: 20px;
+  height: 50px;
+  line-height: 50px;
 }
-
 .addTask {
-    font: 18px bold;
+  font: 18px bold;
 }
-
-.inputTask{
-    height: 30px;
-    line-height: 30px;
-    width:1000px
+.inputTask {
+  height: 30px;
+  line-height: 30px;
+  width: 1000px;
 }
-
 .tip {
-    color: #cc5545;
+  color: #cc5545;
 }
-
 .btn {
-    float: right;
-    height: 30px;
-    line-height: 30px;
-    border: 1px solid black;
-    margin: 0 10px;
-    border:none
+  float: right;
+  height: 30px;
+  line-height: 30px;
+  border: 1px solid black;
+  margin: 0 10px;
+  border: none;
 }
-
 .taskList {
-    font: 18px bold;
+  font: 18px bold;
 }
-
 .task {
-    border: 1px solid #ccc;
-    height: 40px;
-    line-height: 40px;
+  border: 1px solid #ccc;
+  height: 40px;
+  line-height: 40px;
 }
-
 .taskActive {
-    text-decoration: line-through;
-    color: gray;
+  text-decoration: line-through;
+  color: gray;
 }
-
-
 </style>
